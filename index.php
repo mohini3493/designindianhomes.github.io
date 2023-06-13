9882a32dc93f3e2c9c3c54c1fe578691c0838194
Mohiniba Jadeja <mohini3493@gmail.com>
2023-06-13 11:37:00 +0530

    done top picks section

--- index.php
+++ index.php
@@ -2,5 +2,128 @@
 
   <main id="main">
 
+  <!-- ======= Top Picks Section ======= -->
+  <section class="top-picks">
+    <div class="container" data-aos="fade-up">
+
+      <div class="section-header">
+        <h2>Top Picks For You</h2>
+        <p>Impressive Collection For Your Dream Home</p>
+      </div>
+
+      <div class="row gy-4">
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/wooden polishing.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>wooden polishing</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/wooden flooring.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>wooden flooring</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/wall pannaling.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>wall pannaling</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/Vertical Garden.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>Vertical Garden</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/upvc window.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>upvc window</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/tiling.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>tiling</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/sofa.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>sofa</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/pop.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>pop</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/plumbing.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>plumbing</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/kitchen renovation.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>kitchen renovation</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/Interior Lightening.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>Interior Lightening</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/glass partition.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>glass partition</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/exterior cladding.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>exterior cladding</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/electric.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>electric</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/door.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>door</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/beds.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>beds</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/bedroomrenovation.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>bedroom renovation</p>
+          </a>
+        </div>
+        <div class="col-lg-2">
+          <a href="#">
+            <img src="assets/img/bathroom renovation.jpg" class="img-fluid rounded-4 mb-4" alt="">
+            <p>bathroom renovation</p>
+          </a>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- Top Picks Section -->
+
   </main><!-- End #main -->
 <?php include "footer.php" ?>
